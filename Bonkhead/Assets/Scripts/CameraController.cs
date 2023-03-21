ad590de73d021228ae2971fdc411640aad05daed
Maxi Bianchini <bianchinimaxibosko@gmail.com>
2023-03-21 17:41:04 -0300

    Update CameraController.cs
    Camera limits

--- Bonkhead/Assets/Scripts/CameraController.cs
+++ Bonkhead/Assets/Scripts/CameraController.cs
@@ -1,33 +1,14 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
     public GameObject target;
 
-    private float targetPosX;
-    private float targetPosY;
-
-    private float posX;
-    private float posY;
-
     public float rightMax;
     public float leftMax;
 
     public float upMax;
     public float downMax;
-
-    public float speed;
-
-
-    private void Awake()
-    {
-        posX = targetPosX + rightMax;
-        posY = targetPosY + downMax;
-
-        transform.position = Vector3.Lerp(transform.position, new Vector3(posX, posY, -1), 1);
-    }
 
     // Start is called before the first frame update
     void Start()
@@ -38,27 +19,6 @@ public class CameraController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        moveCamera();
-    }
-
-    void moveCamera()
-    {
-       // if (target)
-     //   {
-            targetPosX = target.transform.position.x;
-            targetPosY = target.transform.position.y;
-
-            if(targetPosX > rightMax && targetPosX < leftMax)
-            {
-                posX = targetPosX;
-            }
-
-            if(targetPosY < upMax && targetPosY > downMax) { 
-                posY = targetPosY; 
-            }
-      //  }
-
-        transform.position = Vector3.Lerp(transform.position, new Vector3(posX, posY, -1), speed * Time.deltaTime);
-
+        transform.position = new Vector3(Mathf.Clamp(target.transform.position.x, leftMax, rightMax), Mathf.Clamp(target.transform.position.y, downMax, upMax), transform.position.z);
     }
 }
