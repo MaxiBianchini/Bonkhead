48d74cb9461da96254be24c93f32b37c76b9840d
Maxi Bianchini <bianchinimaxibosko@gmail.com>
2023-03-21 17:40:40 -0300

    Create Parallax Background

---
+++ Bonkhead/Assets/Scripts/BackgroundController.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundController : MonoBehaviour
+{
+    [SerializeField] private Vector2 speedMovement;
+
+    private Vector2 offset;
+    private Material material;
+    private Rigidbody2D rigidbodyPlayer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        material = GetComponent<SpriteRenderer>().material;
+        rigidbodyPlayer = GameObject.FindGameObjectWithTag("Player").GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        offset = (rigidbodyPlayer.velocity.x * 0.1f) * speedMovement * Time.deltaTime;
+        material.mainTextureOffset += offset;
+    }
+}
