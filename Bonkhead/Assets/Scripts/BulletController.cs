32f0de578672a5ccda24b8bc62a2c9d9c8b666cf
Maxi Bianchini <bianchinimaxibosko@gmail.com>
2023-05-04 13:25:59 -0300

    Update BulletController.cs

--- Bonkhead/Assets/Scripts/BulletController.cs
+++ Bonkhead/Assets/Scripts/BulletController.cs
@@ -2,24 +2,30 @@ using UnityEngine;
 
 public class BulletController : MonoBehaviour
 {
-    private int damage = 10;
-
+    // Este método se llama cuando un objeto con un colisionador 2D entra en contacto con el colisionador 2D adjunto a la bala.
     void OnTriggerEnter2D(Collider2D collision)
     {
+        // Si el objeto que ha colisionado con la bala tiene la etiqueta "Player":
         if (collision.gameObject.CompareTag("Player"))
         {
+            // Destruye la bala.
             Destroy(gameObject);
+            // Llama al método "TakeDamage()" del componente "CharacterController" adjunto al objeto que ha colisionado con la bala (el jugador).
             collision.GetComponent<CharacterController>().TakeDamage();
         }
 
+        // Si el objeto que ha colisionado con la bala tiene la etiqueta "Ground":
         if (collision.gameObject.CompareTag("Ground"))
         {
+            // Destruye la bala.
             Destroy(gameObject);
         }
     }
 
+    // Este método se llama cuando el objeto se vuelve invisible para la cámara.
     void OnBecameInvisible()
     {
+        // Destruye la bala.
         Destroy(gameObject);
     }
 }
