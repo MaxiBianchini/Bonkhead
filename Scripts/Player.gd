89d4aae77f571750d63d159b7fdc43109df173f6
MaxiBianchini <bianchinimaxibosko@gmail.com>
2024-02-05 12:00:01 -0300

    Change some code
    Se creó la variable animatedSprite2D

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -5,11 +5,15 @@ var fuerza_salto: float
 var velocidad_movimiento: int
 var primer_salto: bool
 
+var animatedSprite2D
+
 func _ready():
 	gravedad = 2000
 	fuerza_salto = -600
 	velocidad_movimiento = 400
 	primer_salto = false
+	
+	animatedSprite2D = $AnimatedSprite2D
 
 func _physics_process(delta):
 	var input_vector = Vector2.ZERO
@@ -19,38 +23,42 @@ func _physics_process(delta):
 		if is_on_floor():
 			primer_salto = true
 			velocity.y = fuerza_salto
-			$AnimatedSprite2D.stop()
-			$AnimatedSprite2D.play("Jump")
+			animatedSprite2D.stop()
+			animatedSprite2D.play("Jump")
 		else: 
 			if primer_salto:
 				primer_salto = false
 				velocity.y = fuerza_salto
-				$AnimatedSprite2D.stop()
-				$AnimatedSprite2D.play("Double_Jump")
+				animatedSprite2D.stop()
+				animatedSprite2D.play("Double_Jump")
+				
+		if velocity.y > 250 && !is_on_floor():
+			animatedSprite2D.play("Fall")
+			
 	else:
 		velocity.y += gravedad * delta
 	velocity.x = input_vector.x * velocidad_movimiento
 	
 	if velocity.x < 0:
-		$AnimatedSprite2D.flip_h = true
+		animatedSprite2D.flip_h = true
 	if velocity.x > 0:
-		$AnimatedSprite2D.flip_h = false
-	if velocity.y > 0 && !is_on_floor():
-		$AnimatedSprite2D.play("Fall")
+		animatedSprite2D.flip_h = false
+	if velocity.y > 350 && !is_on_floor():
+		animatedSprite2D.play("Fall")
 	
 	move_and_slide()
 	
 	if velocity.x == 0 && velocity.y == 0:
-		$AnimatedSprite2D.play("Idle")
+		animatedSprite2D.play("Idle")
 	
 	if (Input.get_action_strength("ui_right") && velocity.y == 0):
-		$AnimatedSprite2D.play("Walk")
-		$AnimatedSprite2D.position = Vector2(10,0)
+		animatedSprite2D.play("Walk")
+		animatedSprite2D.position = Vector2(10,0)
 		$CollisionShape2D.position = Vector2(10,0)
 	else:
 		if (Input.get_action_strength("ui_left") && velocity.y == 0):
-			$AnimatedSprite2D.play("Walk")
-			$AnimatedSprite2D.position = Vector2(-10,0)
+			animatedSprite2D.play("Walk")
+			animatedSprite2D.position = Vector2(-10,0)
 			$CollisionShape2D.position = Vector2(10,0)
 
 
