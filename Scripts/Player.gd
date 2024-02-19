7a89ab0e7d8c46f1e3749a168f743b5a350f513e
MaxiBianchini <bianchinimaxibosko@gmail.com>
2024-02-19 11:49:10 -0300

    New Animations

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -7,11 +7,19 @@ var fuerza_salto: float
 var velocidad_movimiento: int
 var primer_salto: bool
 
+var puede_dashear: bool
+var esta_dasheando: bool
+var velocidad_dash: int
+
 func _ready():
 	gravedad = 2000
 	fuerza_salto = -600
 	velocidad_movimiento = 400
 	primer_salto = false
+	
+	puede_dashear = true
+	esta_dasheando = false
+	velocidad_dash = 700
 
 func _physics_process(delta):
 	var input_vector = Vector2.ZERO
@@ -29,7 +37,19 @@ func _physics_process(delta):
 				animatedSprite2D.play("Double_Jump")
 	else:
 		velocity.y += gravedad * delta
-	velocity.x = input_vector.x * velocidad_movimiento
+	
+	if Input.is_action_just_pressed("Dash") and puede_dashear:
+		puede_dashear = false
+		esta_dasheando = true
+		animatedSprite2D.stop()
+		animatedSprite2D.play("Dash")
+		$DashTimer.start()
+		$PuedeDashear.start()
+	
+	if esta_dasheando:
+		velocity.x = input_vector.x * velocidad_dash
+	else:
+		velocity.x = input_vector.x * velocidad_movimiento
 	move_and_slide()
 	
 	if velocity.x < 0:
@@ -50,3 +70,9 @@ func _physics_process(delta):
 	if ((Input.get_action_strength("ui_left") || Input.get_action_strength("ui_right")) && is_on_floor()):
 		animatedSprite2D.play("Walk")
 	
+
+func _on_dash_timer_timeout():
+	esta_dasheando = false
+
+func _on_puede_dashear_timeout():
+	puede_dashear = true
