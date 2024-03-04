04b3a846ea59ccf3730cae27ab772bb245bc6c2d
MaxiBianchini <bianchinimaxibosko@gmail.com>
2024-03-04 11:55:10 -0300

    Edit Player
    Se modificó el movimiento de Player y el doble salto, par que lo haga solo en las paredes seleccionadas

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -1,21 +1,24 @@
 extends CharacterBody2D
 
-@onready var animatedSprite2D = $AnimatedSprite2D
-
 var gravedad: int
 var fuerza_salto: float
 var velocidad_movimiento: int
-var primer_salto: bool
+
+var primer_salto_realizado: bool
+var doble_salto_habilitado:bool
 
 var puede_dashear: bool
 var esta_dasheando: bool
 var velocidad_dash: int
 
+@export var animatedSprite2D:AnimatedSprite2D
+@export var rayCast2D:RayCast2D
+
 func _ready():
 	gravedad = 2000
 	fuerza_salto = -550
 	velocidad_movimiento = 200
-	primer_salto = false
+	primer_salto_realizado = false
 	
 	puede_dashear = true
 	esta_dasheando = false
@@ -27,12 +30,11 @@ func _physics_process(delta):
 	
 	if Input.is_action_just_pressed("ui_jump"):
 		if is_on_floor():
-			primer_salto = true
+			primer_salto_realizado = true
 			velocity.y = fuerza_salto
 			animatedSprite2D.play("Jump")
-		else: 
-			if primer_salto:
-				primer_salto = false
+		elif doble_salto_habilitado && primer_salto_realizado:
+				primer_salto_realizado = false
 				velocity.y = fuerza_salto
 				animatedSprite2D.play("Double_Jump")
 	else:
@@ -55,10 +57,14 @@ func _physics_process(delta):
 	
 	if velocity.x < 0:
 		animatedSprite2D.flip_h = true
+		rayCast2D.position.x = 10
+		rayCast2D.target_position.x = -10
 		animatedSprite2D.position = Vector2(-10,0)
 		$CollisionShape2D.position = Vector2(10,0)
-	if velocity.x > 0:
+	elif velocity.x > 0:
 		animatedSprite2D.flip_h = false
+		rayCast2D.position.x = 10
+		rayCast2D.target_position.x = 10
 		animatedSprite2D.position = Vector2(10,0)
 		$CollisionShape2D.position = Vector2(10,0)
 		
@@ -68,9 +74,17 @@ func _physics_process(delta):
 	if velocity.x == 0 && velocity.y == 0:
 		animatedSprite2D.play("Idle")
 	
-	if ((Input.get_action_strength("ui_left") || Input.get_action_strength("ui_right")) && is_on_floor()):
+	if ((Input.get_action_strength("ui_left") || Input.get_action_strength("ui_right")) && is_on_floor()) && !esta_dasheando:
 		animatedSprite2D.play("Walk")
 	
+	if rayCast2D.is_colliding():
+		var col =  rayCast2D.get_collider()
+		if col.is_in_group("Wall"):
+			doble_salto_habilitado = true
+		if col.is_in_group("Floor"):
+			doble_salto_habilitado = false
+	else:
+		doble_salto_habilitado = false
 
 func _on_dash_timer_timeout():
 	esta_dasheando = false
