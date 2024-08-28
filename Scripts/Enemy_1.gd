5900cad18643fbd6b07672497b276fc343bcfa8e
MaxiBianchini <bianchinimaxibosko@gmail.com>
2024-08-28 11:39:04 -0300

    Upgrade Enemy_1
    Se ajustó la animación Angry para que no se reproduzca en loop. Además se ajustaron los momentos donde se ejecuta y terminan de ejecutar ciertas animaciones para asi cambiar su estado

--- Scripts/Enemy_1.gd
+++ Scripts/Enemy_1.gd
@@ -2,29 +2,32 @@ extends CharacterBody2D
 
 # Variables para definir el área de detección rectangular
 var detection_width = 200.0  # Ancho del área de detección
-var detection_height = 100.0  # Altura del área de detección
+var detection_height = 180.0  # Altura del área de detección
 
 # Variables para controlar el movimiento y la física
 const VELOCIDAD = 60.0
 const GRAVEDAD = 2000.0
 var direccion = 1.0
-var stop = false
+var is_stay_angry = false
 
 # Referencias a nodos
 var animated_sprite
 var collision_shape
+var raycast_suelo
 var player
 
 func _ready():
 	# Inicializa las referencias a los nodos
-	animated_sprite = $AnimatedSprite2D
 	collision_shape = $CollisionShape2D
+	raycast_suelo = $RayCast2D
+	
+	animated_sprite = $AnimatedSprite2D
+	animated_sprite.connect("animation_finished", Callable(self, "_on_animation_finished"))
+	# Reproduce la animación de caminar por defecto
+	animated_sprite.play("Walk")
 	
 	# Encuentra al jugador en la escena
 	player = get_node("../Player")
-
-	# Reproduce la animación de caminar por defecto
-	animated_sprite.play("Walk")
 
 func _physics_process(delta):
 	# Actualiza la animación y la posición del enemigo según la dirección
@@ -41,20 +44,20 @@ func _physics_process(delta):
 	else:
 		animated_sprite.position = offset
 		collision_shape.position = offset
-
+		
 	# Aplica la gravedad si no está en el suelo
 	if not is_on_floor():
 		velocity.y += GRAVEDAD * delta
 	
 	# Invierte la dirección si está en la pared o no está en el suelo
-	if is_on_wall() or not is_on_floor():
+	if is_on_wall() or not raycast_suelo.is_colliding():
 		direccion *= -1
-
+		
 	# Controla el movimiento del enemigo
-	if not stop:
+	if animated_sprite.animation != "Angry":
 		velocity.x = direccion * VELOCIDAD
 		move_and_slide()
-
+		
 	# Verifica la proximidad del jugador y cambia la animación y el comportamiento
 	if player:
 		var enemy_position = position
@@ -68,18 +71,28 @@ func _physics_process(delta):
 		
 		# Verificar si el jugador está dentro del área de detección
 		if player_position.x > left_bound and player_position.x < right_bound and player_position.y > top_bound and player_position.y < bottom_bound:
-			animated_sprite.play("Angry")
-			stop = true
+			if not is_stay_angry:
+				animated_sprite.play("Angry")
+				is_stay_angry = true
 			
 			# Actualiza la dirección y posición del enemigo según la posición del jugador
 			if player_position.x < enemy_position.x:
+				if direccion > 0:
+					direccion *= -1
 				animated_sprite.flip_h = true
 				animated_sprite.position = Vector2(-10, 0)
 				collision_shape.position = Vector2(10, 0)
 			else:
+				if direccion < 0:
+					direccion *= -1
 				animated_sprite.flip_h = false
 				animated_sprite.position = Vector2(10, 0)
 				collision_shape.position = Vector2(10, 0)
 		else:
-			animated_sprite.play("Walk")
-			stop = false
+			is_stay_angry = false
+			
+
+func _on_animation_finished():
+	if animated_sprite.animation == "Angry":
+		animated_sprite.play("Walk")
+	
