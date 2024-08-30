b992919212265053b544b79faffb3cb43ddb55de
MaxiBianchini <bianchinimaxibosko@gmail.com>
2024-08-30 16:56:52 -0300

    Upgrade Player
    Se le actualizaron los nombres de las variables y se agregó el salto hacia abajo

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -1,24 +1,32 @@
 extends CharacterBody2D
 
 # Variables para controlar física y movimiento
-var gravedad: int = 2000
-var fuerza_salto: float = -550.0
-var velocidad_movimiento: int = 200
-var velocidad_dash: int = 400
+var gravity: int = 2000
+var jump_force: float = -550.0
+var movement_velocity: int = 200
+var dash_velocity: int = 400
+var fall_through_time: float = 0.8  # Tiempo durante el cual se desactiva la colisión
 
-var primer_salto_realizado: bool = false
-var doble_salto_habilitado: bool = false
-var puede_dashear: bool = true
-var esta_dasheando: bool = false
+var first_jump_completed: bool = false
+var double_jump_enabled: bool = false
+var can_dash: bool = true
+var is_dashing: bool = false
 
-@export var animatedSprite2D: AnimatedSprite2D
-@export var rayCast2D: RayCast2D
+var animatedSprite2D
+var collisionshape2D
+var raycast_wall
+var raycast_floor
 
 func _ready():
+	animatedSprite2D = $AnimatedSprite2D
+	collisionshape2D = $CollisionShape2D
+	raycast_wall = $RayCast2D
+	raycast_floor = $RayCast2D2
+	
 	# Inicialización de variables
-	primer_salto_realizado = false
-	puede_dashear = true
-	esta_dasheando = false
+	first_jump_completed = false
+	can_dash = true
+	is_dashing = false
 
 func _physics_process(delta):
 	# Obtener la entrada del jugador
@@ -27,29 +35,32 @@ func _physics_process(delta):
 	
 	# Manejar el salto
 	if Input.is_action_just_pressed("ui_jump"):
-		if is_on_floor():
-			primer_salto_realizado = true
-			velocity.y = fuerza_salto
+		if Input.is_action_pressed("ui_down"):
+			print("LLEGA AL DOWN")
+			ignore_platform_collision()
+			
+		elif is_on_floor():
+			first_jump_completed = true
+			velocity.y = jump_force
 			animatedSprite2D.play("Jump")
-		elif doble_salto_habilitado and primer_salto_realizado:
-			primer_salto_realizado = false
-			velocity.y = fuerza_salto
+		elif double_jump_enabled and first_jump_completed:
+			first_jump_completed = false
+			velocity.y = jump_force
 			animatedSprite2D.play("Double_Jump")
-	else:
-		# Aplicar gravedad
-		velocity.y += gravedad * delta
-
+	
+	if !is_on_floor():
+		velocity.y += gravity * delta
 	# Manejar el dash
-	if Input.is_action_just_pressed("Dash") and puede_dashear:
-		puede_dashear = false
-		esta_dasheando = true
+	if Input.is_action_just_pressed("Dash") and can_dash:
+		can_dash = false
+		is_dashing = true
 		animatedSprite2D.stop()
 		animatedSprite2D.play("Dash")
 		$DashTimer.start()
-		$PuedeDashear.start()
+		$CanDash.start()
 
 	# Actualizar la velocidad según el estado del dash
-	velocity.x = input_vector.x * (velocidad_dash if esta_dasheando else velocidad_movimiento)
+	velocity.x = input_vector.x * (dash_velocity if is_dashing else movement_velocity)
 	move_and_slide()
 
 	# Actualizar la dirección del sprite y otros elementos según la velocidad
@@ -65,14 +76,14 @@ func update_sprite_direction():
 	var offset = 10
 	if velocity.x < 0:
 		animatedSprite2D.flip_h = true
-		rayCast2D.position.x = offset
-		rayCast2D.target_position.x = -offset
+		raycast_wall.position.x = offset
+		raycast_wall.target_position.x = -offset
 		animatedSprite2D.position.x = -offset
 		$CollisionShape2D.position.x = offset
 	elif velocity.x > 0:
 		animatedSprite2D.flip_h = false
-		rayCast2D.position.x = offset
-		rayCast2D.target_position.x = offset
+		raycast_wall.position.x = offset
+		raycast_wall.target_position.x = offset
 		animatedSprite2D.position.x = offset
 		$CollisionShape2D.position.x = offset
 
@@ -81,21 +92,28 @@ func update_animation():
 		animatedSprite2D.play("Fall")
 	elif velocity.x == 0 and velocity.y == 0:
 		animatedSprite2D.play("Idle")
-	elif (Input.get_action_strength("ui_left") or Input.get_action_strength("ui_right")) and is_on_floor() and not esta_dasheando:
+	elif (Input.get_action_strength("ui_left") or Input.get_action_strength("ui_right")) and is_on_floor() and not is_dashing:
 		animatedSprite2D.play("Walk")
 
 func handle_double_jump():
-	if rayCast2D.is_colliding():
-		var collider = rayCast2D.get_collider()
+	if raycast_wall.is_colliding():
+		var collider = raycast_wall.get_collider()
 		if collider.is_in_group("Wall"):
-			doble_salto_habilitado = true
+			double_jump_enabled = true
 		if collider.is_in_group("Floor"):
-			doble_salto_habilitado = false
+			double_jump_enabled = false
 	else:
-		doble_salto_habilitado = false
+		double_jump_enabled = false
 
+func ignore_platform_collision():
+	collisionshape2D.disabled = true
+	velocity.y = jump_force * -1.5
+	print ("INGORA")
+	await (get_tree().create_timer(fall_through_time))
+	#collisionshape2D.disabled = false
+	print("NO IGNORA")
 func _on_dash_timer_timeout():
-	esta_dasheando = false
+	is_dashing = false
 
-func _on_puede_dashear_timeout():
-	puede_dashear = true
+func _on_can_dash_timeout():
+	can_dash = true
