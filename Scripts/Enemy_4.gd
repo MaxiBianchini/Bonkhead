6287ca283aad97d07d8dc7ff8ae4d31910844fab
MaxiBianchini <bianchinimaxibosko@gmail.com>
2024-09-25 11:45:03 -0300

    Upgrade Enemy_4
    Se hicieron modificaciones en el codigo para ajustar el flip del sprite, se creo la animacion de Hurt

--- Scripts/Enemy_4.gd
+++ Scripts/Enemy_4.gd
@@ -5,7 +5,7 @@ extends CharacterBody2D
 @onready var player = get_node("../Player") # Encuentra al jugador en la escena
 
 var detection_width = 200
-var detection_height = 200
+var detection_height = 400
 var entered_area = false
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -16,7 +16,6 @@ func _ready():
 	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta):
-	
 	if player:
 		var enemy_position = position
 		var player_position = player.position
@@ -31,25 +30,29 @@ func _physics_process(delta):
 		if player_position.x > left_bound and player_position.x < right_bound and player_position.y > top_bound and player_position.y < bottom_bound:
 			print("ENTRRO")
 			entered_area = true
-			animated_sprite.play("Atack")
+			$AnimationPlayer.play("Hurt")
+			#animated_sprite.play("Atack")
 			if player_position.x < enemy_position.x:
 				animated_sprite.flip_h = true
-				animated_sprite.position = Vector2(5, 0)
+				animated_sprite.position = Vector2(-8, 0)
 			else:
 				animated_sprite.flip_h = false
-				animated_sprite.position = Vector2(5, 0)
+				animated_sprite.position = Vector2(7, 0)
 		elif entered_area:
 			entered_area = false
 			print("SALIO")
 			animated_sprite.play("Idle")
 			
+	
+
 func _on_body_entered(body):
 	if body.is_in_group("Player"):
 		print("ENTRRO")
 		animated_sprite.play("Atack")
-		
+	
 
 func _on_body_exited(body):
 	if body.is_in_group("Player"):
 		print("SALIO")
 		animated_sprite.play("Idle")
+	
