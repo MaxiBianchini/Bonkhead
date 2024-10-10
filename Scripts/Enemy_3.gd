c3474a960223b5e8e2894c715ca916a9b70ac621
MaxiBianchini <bianchinimaxibosko@gmail.com>
2024-10-10 12:20:10 -0300

    Upgrade Enemy_3
    Se modificó el movimiento del mismo, no tiene un rango de movimiento, y se agrego un raycast para detectar el suelo

--- Scripts/Enemy_3.gd
+++ Scripts/Enemy_3.gd
@@ -3,40 +3,37 @@ extends CharacterBody2D
 # Referencias a nodos
 @onready var animated_sprite = $AnimatedSprite2D
 @onready var collision_shape = $CollisionShape2D
-@onready var raycast2D = $RayCast2D
+@onready var raycast_detection = $RayCast2D
+@onready var raycast_floor = $RayCast2D2
 
 # Variables para controlar el movimiento del dron
 var movement_velocity: float = 250.0
-var patrol_range: float = 200.0
-var start_position: Vector2
+var start_driving: bool = false
 
 func _ready():
-	#animated_sprite.connect("animation_finished", Callable(self, "_on_animation_finished"))
 	animated_sprite.play("Idle") # Reproduce la animación de caminar por defecto
-	
-	#area2D.connect("body_entered", Callable(self,"_on_body_entered"))
-	start_position = position
 
 func _physics_process(delta):
-	
-
-	if raycast2D.is_colliding():
-		var collider = raycast2D.get_collider()
-		if collider.is_in_group("Player"):
-			animated_sprite.play("Walk")
-	if animated_sprite.animation == "Walk":
-		# Movimiento horizontal del dron
-		position.x += movement_velocity * delta
-		
-		# Control simple de patrullaje (si quieres que vaya de un lado a otro)
-		if position.x > start_position.x + patrol_range or position.x < start_position.x - patrol_range:
-			movement_velocity *= -1  # Cambia de dirección
+	if is_on_wall() or not raycast_floor.is_colliding():
+		movement_velocity *= -1
 		update_sprite_direction()
 	
+	if raycast_detection.is_colliding():
+		var collider = raycast_detection.get_collider()
+		if collider.is_in_group("Player"):
+			animated_sprite.play("Walk")
+			start_driving = true
+	
+	if start_driving:
+		# Movimiento horizontal del dron
+		position.x += movement_velocity  * delta
 
 func update_sprite_direction():
 	if movement_velocity < 0:
 		animated_sprite.flip_h = true
-		
+		raycast_floor.position = Vector2(-40,27.5)
+		raycast_detection.rotate(-3.14159)
 	elif movement_velocity > 0:
 		animated_sprite.flip_h = false
+		raycast_floor.position = Vector2(40,27.5)
+		raycast_detection.rotate(3.14159)
