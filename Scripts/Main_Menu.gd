fd37442e7885ef9bf9856d257c58b76d8f09f016
MaxiBianchini <bianchinimaxibosko@gmail.com>
2024-11-29 17:03:42 -0300

    Edit Menus
    Se realizaron cambios en los distintos menús del juego, cambiando su disposición y contenido

--- Scripts/Main_Menu.gd
+++ Scripts/Main_Menu.gd
@@ -1,18 +1,20 @@
 extends Control
 
 # Conectamos las señales de los botones al script
-@onready var button1 = $CanvasLayer/VBoxContainer/PlayButton
-@onready var button2 = $CanvasLayer/VBoxContainer/OptionButton
-@onready var button3 = $CanvasLayer/VBoxContainer/CreditButton
-@onready var button4 = $CanvasLayer/ExitButton
-@onready var button5 = $CanvasLayer2/VBoxContainer/BackButton
+@onready var Play_Button = $CanvasLayer/VBoxContainer/PlayButton
+@onready var Option_Button = $CanvasLayer/VBoxContainer/OptionButton
+@onready var Credit_Button = $CanvasLayer/VBoxContainer/CreditButton
+@onready var Exit_Button = $CanvasLayer/ExitButton
+@onready var Back_Button = $OthersMenu/BackButtonContainer/BackButton
+
+@onready var background = $CanvasLayer/ButtonBackground
 
 func _ready():
-	button1.connect("pressed", Callable(self, "_on_start_game_pressed"))
-	button2.connect("pressed", Callable(self, "_on_options_pressed"))
-	button3.connect("pressed", Callable(self, "_on_credits_pressed"))
-	button4.connect("pressed", Callable(self, "_on_exit_pressed"))
-	button5.connect("pressed", Callable(self, "_on_back_pressed"))
+	Play_Button.connect("pressed", Callable(self, "_on_start_game_pressed"))
+	Option_Button.connect("pressed", Callable(self, "_on_options_pressed"))
+	Credit_Button.connect("pressed", Callable(self, "_on_credits_pressed"))
+	Exit_Button.connect("pressed", Callable(self, "_on_exit_pressed"))
+	Back_Button.connect("pressed", Callable(self, "_on_back_pressed"))
 
 # Funciones de los botones
 func _on_start_game_pressed():
@@ -22,22 +24,38 @@ func _on_start_game_pressed():
 
 func _on_options_pressed():
 	print("Opciones")
-	#$CanvasLayer.hide()
-	button4.hide()
-	$CanvasLayer2.show()
-	# Aquí puedes abrir una ventana de opciones o cambiar de escena
-	#get_tree().change_scene_to_file("res://Scenes/OptionsMenu.tscn")
+	Exit_Button.hide()
+	Play_Button.hide()
+	Option_Button.hide()
+	Credit_Button.hide()
+	background.set_anchor((SIDE_LEFT),100)
+	background.set_anchor(SIDE_TOP, 200)
+	background.set_anchor(SIDE_RIGHT, 400)
+	background.set_anchor(SIDE_BOTTOM, 350)
+	$OthersMenu.show()
+	$OthersMenu/Options.show()
 
 func _on_credits_pressed():
 	print("Creditos")
-	# Aquí puedes abrir una ventana de creditos o cambiar de escena
-	get_tree().change_scene_to_file("res://Scenes/CreditsMenu.tscn")
+	Exit_Button.hide()
+	$OthersMenu.show()
+	$OthersMenu/Credits.show()
 
 func _on_back_pressed():
 	print("Atras")
-	# Aquí puedes abrir una ventana de creditos o cambiar de escena
-	get_tree().change_scene_to_file("res://Scenes/Main_Menu.tscn")
+	Exit_Button.show()
+	$OthersMenu.hide()
+	$OthersMenu/Options.hide()
+	$OthersMenu/Credits.hide()
 
 func _on_exit_pressed():
 	print("Salir")
 	get_tree().quit()  # Cierra el juego
+
+func _on_fullscreen_checkbutton_toggled(toggled_on: bool) -> void:
+	if toggled_on:
+		# Activa pantalla completa
+		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
+	else:
+		# Cambia a modo ventana
+		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_MAXIMIZED)
