c00613428f8077ec5ac90289c948ad460a6acccb
MaxiBianchini <bianchinimaxibosko@gmail.com>
2024-12-13 16:19:46 -0300

    Update Menus
    Se arreglaron los menus del juego para que hagan su animaciones  muestren los distintos botones y label

--- Scripts/Main_Menu.gd
+++ Scripts/Main_Menu.gd
@@ -6,48 +6,75 @@ extends Control
 @onready var Credit_Button = $CanvasLayer/ButtonsContainer/CreditButton
 @onready var Exit_Button = $CanvasLayer/ExitButton
 @onready var Back_Button = $OthersMenu/BackButtonContainer/BackButton
+@onready var button_background = $CanvasLayer/ButtonBackground
 
 func _ready():
-	Play_Button.connect("pressed", Callable(self, "_on_start_game_pressed"))
-	Option_Button.connect("pressed", Callable(self, "_on_options_pressed"))
-	Credit_Button.connect("pressed", Callable(self, "_on_credits_pressed"))
-	Exit_Button.connect("pressed", Callable(self, "_on_exit_pressed"))
-	Back_Button.connect("pressed", Callable(self, "_on_back_pressed"))
+	# Conectamos las señales de los botones a sus respectivas funciones
+	Play_Button.pressed.connect(_on_start_game_pressed)
+	Option_Button.pressed.connect(_on_options_pressed)
+	Credit_Button.pressed.connect(_on_credits_pressed)
+	Exit_Button.pressed.connect(_on_exit_pressed)
+	Back_Button.pressed.connect(_on_back_pressed)
 
-# Funciones de los botones
+# Inicia el juego cambiando a la escena del primer nivel
 func _on_start_game_pressed():
 	print("Iniciar juego")
-	# Cambia de escena o realiza alguna acción para iniciar el juego
 	get_tree().change_scene_to_file("res://Scenes/Level_1.tscn")
 
+# Muestra el menú de opciones con una animación
 func _on_options_pressed():
-	print("Opciones")
-	Exit_Button.hide()
-	$CanvasLayer/ButtonsContainer.hide()
-	$OthersMenu.show()
-	$OthersMenu/Options.show()
+	show_main_menu(false)
+	await animate_menu(true)
+	show_other_menu(true)
 
+# Muestra los créditos con la misma animación de menú
 func _on_credits_pressed():
-	print("Creditos")
-	Exit_Button.hide()
-	$CanvasLayer/ButtonsContainer.hide()
-	$OthersMenu.show()
-	$OthersMenu/Credits.show()
+	show_main_menu(false)
+	await animate_menu(true)
+	show_other_menu(false)
 
+# Regresa al menú principal desde cualquier menú secundario con animación
 func _on_back_pressed():
-	print("Atras")
-	Exit_Button.show()
-	$CanvasLayer/ButtonsContainer.show()
 	$OthersMenu.hide()
 	$OthersMenu/Options.hide()
 	$OthersMenu/Credits.hide()
+	await animate_menu(false)
+	show_main_menu(true)
 
+# Cierra el juego
 func _on_exit_pressed():
-	print("Salir")
-	get_tree().quit()  # Cierra el juego
+	get_tree().quit()
 
+# Función para manejar la animación del menú, tomando un parámetro para determinar si es de entrada o salida
+func animate_menu(enter: bool):
+	var size = Vector2(1430, 1080) if enter else Vector2(540, 700)
+	var position = Vector2(245, 0) if enter else Vector2(690, 305)
+	var tween = create_tween()
+	tween.tween_property(button_background, "size", size, 0.5)#.set_trans(Tween.TRANS_SINE)
+	tween.parallel().tween_property(button_background, "position", position, 0.5)
+	await tween.finished
+
+# Centraliza la lógica para mostrar el otro menú
+func show_other_menu(enter: bool):
+	if enter:
+		$OthersMenu/Options.show()
+	else:
+		$OthersMenu/Credits.show()
+	$OthersMenu.show()
+
+# Centraliza la lógica para mostrar u ocultar el menú principal
+func show_main_menu(enter: bool):
+	if enter:
+		Exit_Button.show()
+		$CanvasLayer/ButtonsContainer.show()
+	else:
+		$CanvasLayer/Label.hide()
+		$CanvasLayer/ExitButton.hide()
+		$CanvasLayer/ButtonsContainer.hide()
+
+# Maneja el cambio entre modo pantalla completa y ventana
 func _on_fullscreen_checkbutton_toggled(toggled_on: bool) -> void:
 	if toggled_on:
-		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN) # Activa pantalla completa
+		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
 	else:
-		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_MAXIMIZED) # Cambia a modo ventana
+		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_MAXIMIZED)
--- Scripts/Pause_Menu.gd
+++ Scripts/Pause_Menu.gd
@@ -8,20 +8,31 @@ extends Node
 @onready var Option_Button = $PauseMenu/VBoxContainer/OptionButton
 @onready var Resume_Button = $PauseMenu/VBoxContainer/ResumeButtom
 @onready var Back_Button = $OptionsMenu/BackButtonContainer/BackButton
+@onready var button_background = $PauseMenu/ButtonBackground
 
-func _ready() -> void:
-	MainMenu_Button.connect("pressed", Callable(self, "_on_mainmenu_pressed"))
-	Option_Button.connect("pressed", Callable(self, "_on_options_pressed"))
-	Resume_Button.connect("pressed", Callable(self, "_on_resume_pressed"))
-	Back_Button.connect("pressed", Callable(self, "_on_back_pressed"))
+func _ready() -> void:#Play_Button.pressed.connect(_on_start_game_pressed)
+	MainMenu_Button.pressed.connect(_on_mainmenu_pressed)
+	Option_Button.pressed.connect(_on_options_pressed)
+	Resume_Button.pressed.connect(_on_resume_pressed)
+	Back_Button.pressed.connect(_on_back_pressed)
 
 func _on_mainmenu_pressed():
 	get_tree().paused = false  # Reanuda el juego
 	# Cambia de escena o realiza alguna acción para iniciar el juego
 	get_tree().change_scene_to_file("res://Scenes/Main_Menu.tscn")
 
+# Función para manejar la animación del menú, tomando un parámetro para determinar si es de entrada o salida
+func animate_menu(enter: bool):
+	var size = Vector2(1430, 1080) if enter else Vector2(540, 700)
+	var position = Vector2(245, 0) if enter else Vector2(690, 190)
+	var tween = create_tween()
+	tween.tween_property(button_background, "size", size, 0.5)#.set_trans(Tween.TRANS_SINE)
+	tween.parallel().tween_property(button_background, "position", position, 0.5)
+	await tween.finished
+
 func _on_options_pressed():
-	$PauseMenu.hide()
+	$PauseMenu/VBoxContainer.hide()
+	await animate_menu(true)
 	$OptionsMenu.show()
 
 func _on_resume_pressed():
@@ -29,8 +40,9 @@ func _on_resume_pressed():
 	get_tree().paused = false  # Reanuda el juego
 
 func _on_back_pressed():
-	$PauseMenu.show()
 	$OptionsMenu.hide()
+	await animate_menu(false)
+	$PauseMenu/VBoxContainer.show()
 
 # Método para manejar la pausa
 func _input(event):
@@ -53,7 +65,6 @@ func _on_return_to_menu_pressed():
 # Continuar el juego (ocultar el menú)
 func _on_continue_pressed():
 	toggle_pause_menu()
-	
 
 func _on_fullscreen_checkbutton_toggled(toggled_on: bool) -> void:
 	if toggled_on:
