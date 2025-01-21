2eeb027b6fe3361c6a3b053bcda47f38be3a14a0
Maxi Bianchini <bianchinimaxibosko@gmail.com>
2025-01-21 12:48:43 -0300

    Upgrade Enemy_4
    Se le ajustó el area de accion, se le agregó el disparo

--- Scripts/Enemy_4.gd
+++ Scripts/Enemy_4.gd
@@ -6,9 +6,14 @@ extends CharacterBody2D
 
 @onready var player = get_node("../Player") # Encuentra al jugador en la escena
 
-var detection_width: int = 400
+var bullet_scene = preload("res://Prefabs/Bullet_1.tscn")
+
+var detection_width: int = 10000
 var detection_height: int = 180
-var entered_area: bool = false
+var can_shoot: bool = false
+
+var bullet_dir = Vector2.RIGHT
+var bullet_offset = Vector2(-15,5)
 
 # Variables para controlar la vida
 var lives: int = 3
@@ -37,18 +42,31 @@ func _physics_process(delta):
 		
 		# Verificar si el jugador está dentro del área de detección
 		if player_position.x > left_bound and player_position.x < right_bound and player_position.y > top_bound and player_position.y < bottom_bound:
-			entered_area = true
-			animated_sprite.play("Atack")
 			if player_position.x < enemy_position.x:
 				animated_sprite.flip_h = true
 				animated_sprite.position = Vector2(-8, 0)
+				bullet_offset = Vector2(-25,5)
+				bullet_dir = Vector2.LEFT
 			else:
 				animated_sprite.flip_h = false
 				animated_sprite.position = Vector2(7, 0)
-		elif entered_area:
-			entered_area = false
-			animated_sprite.play("Idle")
+				bullet_dir = Vector2.RIGHT
+				bullet_offset = Vector2(25,5)
+				
+		if can_shoot:
+			shoot_bullet()
+			can_shoot = false
+			$Can_Shoot.start()
+		
 
+func shoot_bullet():
+	var bullet = bullet_scene.instantiate() # Instancia la bala
+	
+	 # Posición final de la bala y dirección
+	bullet.position = position + bullet_offset
+	bullet.direction = bullet_dir
+	
+	get_tree().current_scene.add_child(bullet) # Añade la bala a la escena actual
 
 # Controlador del Daño
 func take_damage():
@@ -64,13 +82,19 @@ func take_damage():
 func _on_body_entered(body):
 	if body.is_in_group("Player"):
 		animated_sprite.play("Atack")
+		can_shoot = true
 
 
 func _on_body_exited(body):
 	if body.is_in_group("Player"):
 		animated_sprite.play("Idle")
+		can_shoot = false
 
 
 func _on_animation_finished():
 	if animated_sprite.animation == "Death":
 		queue_free()
+
+func _on_can_shoot_timeout() -> void:
+	print("PASÖ POR EL TIMER")
+	can_shoot = true
