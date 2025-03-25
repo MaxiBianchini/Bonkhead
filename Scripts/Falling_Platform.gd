06b44e99e7285b40822c0258d70474fb310d267d
Maxi Bianchini <bianchinimaxibosko@gmail.com>
2025-03-25 17:01:06 -0300

    Update Falling Platform
    Se acomodó la colision, se ajustó el sprite

--- Scripts/Falling_Platform.gd
+++ Scripts/Falling_Platform.gd
@@ -25,16 +25,6 @@ func _on_body_entered(body: Node) -> void:
 			add_child(collapse_timer)
 			collapse_timer.start()
 
-func _on_body_exited(body: Node) -> void:
-	# Verificar si el cuerpo que salió es el jugador
-	if body.name == "Player":
-		player_on_platform = false
-		# Detener y eliminar el temporizador si el jugador sale
-		if collapse_timer != null:
-			collapse_timer.stop()
-			collapse_timer.queue_free()
-			collapse_timer = null
-
 func _start_falling() -> void:
 	# Solo iniciar el colapso si el jugador sigue en la plataforma
 	if player_on_platform:
@@ -44,8 +34,6 @@ func _start_falling() -> void:
 func _physics_process(delta: float) -> void:
 	# Si la plataforma está cayendo, verificar colisión con el suelo
 	if falling:
-		for body in get_colliding_bodies():
-			if body.is_in_group("Floor") and linear_velocity.length() < 0.1:
-				# Eliminar la plataforma cuando toca el suelo y se detiene
-				queue_free()
-				break
+		# Eliminar la plataforma luego de unos segundos de haber caido
+		await get_tree().create_timer(2.0).timeout
+		queue_free()
