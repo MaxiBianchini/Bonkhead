c72dc5fcb4e38ae115e4033d898261a191631b9b
Maxi Bianchini <bianchinimaxibosko@gmail.com>
2025-03-27 11:57:11 -0300

    Update transitions

--- Scripts/Scenes_Transitions.gd
+++ Scripts/Scenes_Transitions.gd
@@ -6,4 +6,3 @@ func change_scene(target: String) -> void:
 	get_tree().change_scene_to_file(target)
 	$AnimationPlayer.play_backwards("fade_in")
 	await $AnimationPlayer.animation_finished
-	queue_free()
