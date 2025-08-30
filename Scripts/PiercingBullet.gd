173de811e02042f6b18d94f2535f819a72349c4a
MaxiBianchini <bianchinimaxibosko@gmail.com>
2025-08-30 17:25:23 -0300

    Add new PiercingBullet

---
+++ Scripts/PiercingBullet.gd
@@ -0,0 +1,59 @@
+extends Area2D
+
+@export var speed: float = 400.0
+# Esta es la variable clave: ¿cuántos enemigos puede atravesar antes de desaparecer?
+@export var max_pierces: int = 3
+@export var time: float = 3
+@onready var life_timer: Timer = $Timer
+
+@onready var sprite: Sprite2D = $Sprite2D
+var direction: Vector2 = Vector2.RIGHT
+var shooter: Node2D = null
+var mask: int = 1
+
+func _ready() -> void:
+	if direction.y != 0:
+		sprite.rotation_degrees = -90
+	elif direction.x < 0:
+		sprite.flip_h = true
+	
+	set_collision_mask_value(mask,true)
+	life_timer.start(time)
+	await life_timer.timeout
+	queue_free()
+
+func _physics_process(delta: float) -> void:
+	# Movimiento en línea recta, simple y rápido.
+	position += direction.normalized() * speed * delta
+
+func _on_body_entered(body: Node2D) -> void:
+	# Ignoramos la colisión con quien nos disparó.
+	if body == shooter:
+		return
+	
+	# Si choca con el suelo o una pared, se destruye.
+	if body.is_in_group("Floor") or body.is_in_group("Grabbable Wall") or body.is_in_group("Jumpeable Wall"):
+		queue_free()
+	
+	# Si choca con un enemigo...
+	if body.is_in_group("Enemy"):
+		# Le hacemos daño.
+		if body.has_method("take_damage"):
+			body.take_damage()
+		
+		# Reducimos el contador de perforaciones.
+		max_pierces -= 1
+		
+		# Si ya no le quedan perforaciones, se destruye.
+		if max_pierces <= 0:
+			queue_free()
+
+func set_mask(number: int) -> void:
+	mask = number
+	
+# Funciones para que el jugador nos configure
+func set_shooter(shooter_node: Node2D) -> void:
+	shooter = shooter_node
+
+func set_direction(dir: Vector2) -> void:
+	direction = dir
