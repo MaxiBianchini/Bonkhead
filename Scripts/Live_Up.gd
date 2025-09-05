4b543c24ae33ab8962f3aff74422d9273558c2f2
Maxi Bianchini <bianchinimaxibosko@gmail.com>
2025-09-05 18:41:04 -0300

    Add new SFX

--- Scripts/Live_Up.gd
+++ Scripts/Live_Up.gd
@@ -4,4 +4,5 @@ func _on_body_entered(body: Node2D) -> void:
 	if body.name == "Player":
 		if body.increase_life():
 			$AudioStreamPlayer2D.play()
+			await  $AudioStreamPlayer2D.finished
 			queue_free()
--- Scripts/PageComic.gd
+++ Scripts/PageComic.gd
@@ -5,4 +5,6 @@ signal winLevel
 func _on_body_entered(body: Node2D) -> void:
 	if body.is_in_group("Player"):
 		emit_signal("winLevel")
+		$AudioStreamPlayer2D.play()
+		await $AudioStreamPlayer2D.finished
 		queue_free()
